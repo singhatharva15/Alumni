eba072bc179d37fba3655c9fd0f06500df57c14b
Sushil333 <sushilbhardwaj70@gmail.com>
2022-08-10 00:42:25 +0530

    Added dj-database-url

--- dashboard/settings.py
+++ dashboard/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+import dj_database_url
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -108,6 +111,10 @@ DATABASES = {
     }
 }
 
+
+# online database connection config
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
